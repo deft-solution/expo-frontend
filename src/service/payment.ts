e28c47970f31df714fec9287ea2b47810f0d5709
YanPuty <yanputy123@gmail.com>
2024-11-26 07:27:18 +0700

    Update UI Checkout and Payment Process

--- src/service/payment.ts
+++ src/service/payment.ts
@@ -1,5 +1,10 @@
-import { ICreatePaymentQRCode, IGenerateQRCodeSuccess, IVerifyTransactionSuccess } from '@/models/Payment';
-import { POST } from '@Core';
+import { getWonderPassToken } from '@/helper';
+import {
+  ICreatePaymentQRCode,
+  IGenerateQRCodeSuccess,
+  IVerifyTransactionSuccess,
+} from '@/models/Payment';
+import { GET, POST } from '@Core';
 
 export const verifyPaymentTransaction = (
   transactionId: string
@@ -13,4 +18,11 @@ export const createQRCodePayment = (
 ): Promise<IGenerateQRCodeSuccess> => {
   const API_URL = '/api/payments/v1/qrcode';
   return POST<IGenerateQRCodeSuccess, ICreatePaymentQRCode>(API_URL, param);
+};
+
+export const verifyPaymenyTransaction = (
+  transactionId: string
+): Promise<IVerifyTransactionSuccess> => {
+  const API_URL = '/api/payments/v1/status';
+  return POST<IVerifyTransactionSuccess, { transactionId: string }>(API_URL, { transactionId });
 };
