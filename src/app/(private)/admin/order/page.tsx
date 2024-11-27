28fea9d23e0271022d56e3f95ab2c571c25b79f8
ung monita <iamungmonita@gmail.com>
2024-11-27 20:31:04 +0900

    change event ID to Name

--- src/app/(private)/admin/order/page.tsx
+++ src/app/(private)/admin/order/page.tsx
@@ -124,7 +124,7 @@ function Page() {
             </th>
 
             <th className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
-              Event ID
+              Event Name
             </th>
 
             <th className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
