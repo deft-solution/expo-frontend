e36c2037b46918939d08babd76dce9b2445f2ce0
Puty YAN <yan.puty@ababank.com>
2024-11-29 14:49:05 +0700

    Update Checkout and Selected Booth

--- src/components/partials/BoothSelection/index.tsx
+++ src/components/partials/BoothSelection/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useEffect, useRef, useState } from 'react';
+
 import { useBoothSelection } from '@/context/BoothSelectionContext';
 
 interface BootSelectionTypeProps {
